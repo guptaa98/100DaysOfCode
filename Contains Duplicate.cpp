4c741ec6187627d811af649030cb28b49e30aa7b
Prashasti Gupta <prashasti708@gmail.com>
2020-10-06 20:44:13 +0530

    Create Contains Duplicate.cpp

---
+++ Contains Duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) 
+    {
+        sort(nums.begin(),nums.end());
+        int i = 0;
+        for(i = 1 ; i< nums.size() ; i++)
+        {
+            if (nums[i]==nums[i-1])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
