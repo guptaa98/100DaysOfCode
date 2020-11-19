7321752ee8ff7268df61a60cfd8aeb8625f91bf7
Prashasti Gupta <prashasti708@gmail.com>
2020-11-19 23:35:44 +0530

    Create Decode String.cpp

---
+++ November_Leetcode_Challenge/Decode String.cpp
@@ -0,0 +1,64 @@
+
+class Solution {
+public:
+    string decodeString(string s) 
+    {
+        string result;
+        stack<string> st;
+        for(int i = 0 ;i < s.size(); i++)
+        while(i<s.size())    
+        {
+            if(isdigit(s[i]))
+            {
+                int num = 0;
+                while(i<s.size() && isdigit(s[i]))
+                {
+                    num = num * 10 + (s[i] - '0');
+                    i++;
+                }
+                //i--;
+                st.push(to_string(num));
+            }
+            else if(isalpha(s[i]))
+            {
+                string word = "";
+                while(i<s.size() && isalpha(s[i]))
+                {
+                    word = word + s[i];
+                    i++;
+                }
+                //i--;
+                st.push(word);
+            }
+            else if(s[i] == '[')
+            {
+                st.push("[");
+                i++;
+            }
+            else if(s[i] == ']')
+            {
+                string word, current;
+                while(st.top() != "[")
+                {
+                    word = st.top() + word;
+                    st.pop();
+                }
+                st.pop();
+                int times = stoi(st.top());  
+                st.pop();
+                while(times--)
+                {
+                    current += word;
+                }
+                st.push(current);
+                i++;
+            }
+        }
+        while(!st.empty())
+        {
+            result = st.top() + result; 
+            st.pop();
+        }
+        return result;
+    }
+};
